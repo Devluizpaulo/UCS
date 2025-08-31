1d4058e56ca195015d18d05e0427ef150db89e5a
Luiz Paulo Gonçalves Miguel de Jesus <luizao16@gmail.com>
2025-08-31 02:12:33 +0000

    a imagem se encontra src/public/image/ucs.png

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,8 @@ import { MainLayout } from '@/components/main-layout';
 import { DashboardPage } from '@/components/dashboard-page';
 
 export default function Home() {
-  // Since we are using a placeholder image, we need to provide the correct path.
-  // The image is added to the public folder, so we can reference it directly.
-  const ucsCoinImageUrl = '/ucs-coin.png';
+  // A imagem está na pasta `public`, então podemos referenciá-la diretamente no path.
+  const ucsCoinImageUrl = '/image/ucs.png';
 
   return (
     <MainLayout>
