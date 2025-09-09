70919b55ae9c791535e0ce99ac683fe9ca0c538c
Devluizpaulo <136992113+Devluizpaulo@users.noreply.github.com>
2025-09-09 14:06:14 -0300

    Update firebase-admin-config.ts

--- src/lib/firebase-admin-config.ts
+++ src/lib/firebase-admin-config.ts
@@ -16,7 +16,7 @@ if (!admin.apps.length) {
             FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
             FIREBASE_PRIVATE_KEY_ID: process.env.FIREBASE_PRIVATE_KEY_ID,
             FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
-            FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY
+            FIREBASE_SERVICE_ACCOUNT: process.env.FIREBASE_SERVICE_ACCOUNT
         };
 
         // Check for missing environment variables
