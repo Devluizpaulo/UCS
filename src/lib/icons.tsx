0ac938868519a69f94e5531d0bc7d05b9fc5bc2f
Luiz Paulo Gonçalves Miguel de Jesus <luizao16@gmail.com>
2025-09-22 21:15:53 +0000

    dola esta fora revise a coleção usd

--- src/lib/icons.tsx
+++ src/lib/icons.tsx
@@ -1,13 +1,11 @@
 
 import type { CommodityPriceData } from '@/lib/types';
-import { DollarSign, LandPlot, TreePine, Droplets, HelpCircle, Euro, Beef, Wheat, Bean, Sigma, Gem } from 'lucide-react';
+import { DollarSign, LandPlot, TreePine, Droplets, HelpCircle, Euro, Beef, Wheat, Bean } from 'lucide-react';
 
 export const getIconForCategory = (asset?: CommodityPriceData) => {
     if (!asset) return HelpCircle;
     
     switch (asset.id) {
-        case 'ucs': return Gem;
-        case 'ucs_ase': return Gem;
         case 'eur': return Euro;
         case 'boi_gordo': return Beef;
         case 'milho': return Wheat;
@@ -15,9 +13,6 @@ export const getIconForCategory = (asset?: CommodityPriceData) => {
         case 'usd': return DollarSign;
         case 'madeira': return TreePine;
         case 'carbono': return Droplets;
-        case 'agua': return Droplets;
-        case 'custo_agua': return Droplets;
-        case 'pdm': return Sigma;
         default:
             switch (asset.category) {
                 case 'exchange': return DollarSign;
