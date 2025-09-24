709eb74261cd9d1ce2c18840931f8a696bb9ddad
Luiz Paulo Gonçalves Miguel de Jesus <luizao16@gmail.com>
2025-09-24 22:49:18 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "ucs-index-tracker",
+  "appId": "1:458013275204:web:9da7774b1648d15d527811",
+  "apiKey": "AIzaSyB3-5OjQnMteTZS-nYaORUI3-dQP19DvSw",
+  "authDomain": "ucs-index-tracker.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "458013275204"
+};
